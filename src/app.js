0e025d91a18794f270b9b50f156c9f800af592d0
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-07 16:03:58 -0300

    Config db connection

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import routes from "./routes";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+class App {
+  constructor() {
+    this.server = express();
+    this.connectDatabase();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+  routes() {
+    this.server.use(routes);
+  }
+  async connectDatabase() {
+    try {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Successfully connected to mongo");
+    } catch (error) {
+      console.log(`error to connect to Atlas DB: ${error}`);
+    }
+  }
+}
+
+export default new App().server;

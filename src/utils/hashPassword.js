eb5081042de76e9718cf83c8d34a0dce9024fa47
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-11 16:18:25 -0300

    Decreasing the salt of the hash

--- src/utils/hashPassword.js
+++ src/utils/hashPassword.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 
 module.exports = async (password) => {
   try {
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(6);
     const passwordHashed = await bcrypt.hash(password, salt);
 
     return passwordHashed;
